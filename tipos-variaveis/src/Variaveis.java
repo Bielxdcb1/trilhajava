ad4c3de29e281e32733ccf31983f22786afcec45
Gabicode123 <gabriel.bombardino@gmail.com>
2024-06-14 19:58:09 -0300

    Variaveis

---
+++ tipos-variaveis/src/Variaveis.java
@@ -0,0 +1,23 @@
+public class App {
+    public static void main(String[] args) throws Exception {   
+    //tipos primitivos
+    //a classe String representa texto na aplicaão
+
+    String meuNome = "Gabriel"
+    
+        double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+ int numero = 5;
+ 
+numero = 10;
+
+System.out.print(numero);
+
+final double VALOR_DE_PI = 3.14;
+
+    }
+}
